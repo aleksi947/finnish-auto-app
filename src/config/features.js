187a5938f3a217ee79caf0ad7b9d077f70ff4dd3
aleksi947 <120279683+aleksi947@users.noreply.github.com>
2026-07-18 20:35:53 +0200

    Add subscription feature flag

---
+++ src/config/features.js
@@ -0,0 +1,3 @@
+// Keep subscription code ready, but hide the feature during the free beta.
+// Change to true when subscriptions are ready to be shown to users.
+export const SUBSCRIPTIONS_ENABLED = false;
